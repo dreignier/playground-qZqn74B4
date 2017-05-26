fff81af096b4bab9682620d1829308d711382c86
David Reignier <david.reignier@gmail.com>
2017-05-26 19:10:40 +0200

    Try to fix a snippet

--- snippets/part2.01.js
+++ snippets/part2.01.js
@@ -1,6 +1,6 @@
 let fs = require('fs');
 
-fs.readFile('test.txt', 'utf8', function(error, data) {
+fs.readFile('./test.txt', 'utf8', function(error, data) {
     if (error) {
          throw error;
     }
