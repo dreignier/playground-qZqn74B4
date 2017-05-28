71787ec0b99623d9e393b0bc616068bdb063c265
David Reignier <david.reignier@gmail.com>
2017-05-28 11:21:35 +0200

    Finish part4

--- markdowns/part4.md
+++ markdowns/part4.md
@@ -1,1 +1,6 @@
-@[Asynchronous code using `setTimeout`]({"stubs":["part4/code.js"], "command":"node part4/part4.js", "layout": "aside"})
+The given code is synchronous. It call `callback1` one time and `callback2` three times. You must modify this code to make it asynchronous with the following rules:
+
+* `callback1` must be called only one time 2 seconds later
+* `callback2` must be called three times with an interval of 1 second.
+
+@[Change the code to make it asynchronous]({"stubs":["part4/code.js"], "command":"node part4/part4.js", "layout": "aside"})
--- snippets/part4/part4.js
+++ snippets/part4/part4.js
@@ -1,1 +1,66 @@
 let job = require('./code.js');
+    success = true;
+
+function now() {
+    return new Date().getTime();
+}
+
+let counter1 = 0,
+    callback1Timer = now();
+
+function callback1() {
+    if (counter1) {
+        success = false;
+        console.log('TECHIO> message callback1 called more than one time');
+    }
+
+    counter1 += 1;
+
+    let time = now() - callback1Timer;
+
+    if (time < 1800) {
+        success = false;
+        console.log('TECHIO> message callback1 called too early');
+    } else if (time > 2200) {
+        success = false;
+        console.log('TECHIO> message callback1 called too late');
+    }
+}
+
+let counter2 = 0,
+    callback2Timer = now();
+
+function callback2() {
+    if (counter2 >= 3) {
+        success = false;
+        console.log('TECHIO> message callback1 called more than three times');
+    }
+
+    counter2 += 1;
+
+    let time = now() - callback2Timer;
+
+    if (time < 800) {
+        success = false;
+        console.log('TECHIO> message callback2 called too early');
+    } else if (time > 1200) {
+        success = false;
+        console.log('TECHIO> message callback2 called too late');
+    }
+
+    callback2Timer = now();
+}
+
+setTimeout(function() {
+    if (counter1 < 1) {
+        success = false;
+        console.log('TECHIO> message callback1 never called');
+    }
+
+    if (counter2 < 3) {
+        success = false;
+        console.log('TECHIO> message callback2 called only ' + counter2 + ' times');
+    }
+
+    console.log('TECHIO> success ' + success);
+}, 6000);
