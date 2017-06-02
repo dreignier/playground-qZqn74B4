bd7be7255ce0b7dc736392b82182df7dcc580d37
David Reignier <david.reignier@gmail.com>
2017-06-02 18:55:52 +0200

    Add part9

--- part9/referee.js
+++ part9/referee.js
@@ -1,2 +1,90 @@
 let job = require('./code.js');
 
+test('test', 0, false, 'error')
+
+.then(function() {
+    return test(3, 1000, true, 'odd');
+})
+
+.then(function() {
+    return test(6, 2000, false, 'even');
+})
+
+.then(function() {
+    console.log('TECHIO> success true');
+})
+
+.catch(function() {
+    console.log('TECHIO> success false');
+});
+
+
+function test(value, delay, resolved, data) {
+    return new Promise(function(resolve, reject) {
+        console.log('TECHIO> message Testing with data ' + value + ' (' + (typeof value) + ')');
+
+        var start = now(),
+            promise = job(value),
+            success = false,
+            finished = false;
+
+        if (!isPromise(promise)) {
+            console.log('TECHIO> message Result is not a promise');
+            reject();
+            return;
+        }
+
+        promise
+
+        .then(function(data) {
+            if (resolved) {
+                success = true;
+            } else {
+                console.log('TECHIO> message Promise is resolved but it should be rejected');
+            }
+        })
+
+        .catch(function(data) {
+            if (!resolved) {
+                console.log('TECHIO> message Promise is rejected but it should be resolved');
+            } else {
+                success = true;
+            }
+        })
+
+        .then(function() {
+            finished = true;
+        })
+
+        setTimeout(function() {
+            if (!finished) {
+                console.log('TECHIO> message Promise never resolved or rejected');
+            }
+
+            if (success) {
+                resolve();
+            } else {
+                reject();
+            }
+        }, delay + 100);
+    });
+}
+
+
+function isPromise(obj) {
+    return isObject(obj) && isFunction(obj.then) && isFunction(obj.catch);
+}
+
+function isObject(obj) {
+    let type = typeof obj;
+    return type === 'function' || type === 'object' && !!obj;
+}
+
+function isFunction(obj) {
+    let type = typeof obj;
+    return type === 'function' && !!obj;
+}
+
+function now() {
+    return new Date().getTime();
+}
