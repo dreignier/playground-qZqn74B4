4e7c082601111db1999c632dcb3bc1ed98e24deb
David Reignier <david.reignier@gmail.com>
2017-06-12 19:39:45 +0200

    test for part16

--- part16/referee.js
+++ part16/referee.js
@@ -3,14 +3,14 @@ let job = require('./code'),
 
 Promise.all([
     testSuccess.apply(null, '1 jsmith USA John Smith jsmith@gmail.com'.split(' ')),
-    testSuccess.apply(null, '2 kmartin France Kevin Martin darkwarrior73@msn.fr'.split(' ')),
+    /*testSuccess.apply(null, '2 kmartin France Kevin Martin darkwarrior73@msn.fr'.split(' ')),
     testSuccess.apply(null, '3 egringo Mexico Erico Gringo elgringo@gmail.com'.split(' ')),
     testSuccess.apply(null, '4 gkruger Germany Gunter Kruger guntertag@hotmail.com'.split(' ')),
     testError(5, 'Error central'),
     testError(6, 'Error db1'),
     testError(7, 'Error db2'),
     testError(8, 'Error db3'),
-    testError(9, 'Error vault')
+    testError(9, 'Error vault')*/
 ])
 
 .then(function() {
