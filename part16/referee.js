d90629c43dbeefa21921dba15b16e9a538a44331
David Reignier <david.reignier@gmail.com>
2017-06-12 16:51:24 +0200

    Finish the referee for part16

--- part16/referee.js
+++ part16/referee.js
@@ -22,12 +22,135 @@ Promise.all([
 });
 
 
-function testSuccess(id, username, contry, firstname, lastname, email) {
+function testSuccess(id, username, country, firstname, lastname, email) {
     return new Promise(function(resolve, reject) {
+        let promise = job();
+
+        if (!isPromise(promise)) {
+            console.error('Test with id', id + ': Result is not a promise');
+            reject();
+            return;
+        }
+
+        let success = false,
+            called = false;
+
+        job
+
+        .then(function(data) {
+            success = true;
+
+            if (data.id !== id) {
+                console.error('Test with id', id + ': The returned id is', data.id, 'but it should be', id);
+                success = false;
+            }
+
+            if (data.username !== username) {
+                console.error('Test with username', username + ': The returned username is', data.username, 'but it should be', username);
+                success = false;
+            }
+
+            if (data.country !== country) {
+                console.error('Test with country', country + ': The returned country is', data.country, 'but it should be', country);
+                success = false;
+            }
+
+            if (data.firstname !== firstname) {
+                console.error('Test with firstname', firstname + ': The returned firstname is', data.firstname, 'but it should be', firstname);
+                success = false;
+            }
+
+            if (data.lastname !== lastname) {
+                console.error('Test with lastname', lastname + ': The returned lastname is', data.lastname, 'but it should be', lastname);
+                success = false;
+            }
+
+            if (data.email !== email) {
+                console.error('Test with email', email + ': The returned email is', data.email, 'but it should be', email);
+                success = false;
+            }
+        })
+
+        .catch(function() {
+            console.error('Test with id', id + ': The should be resvoled');
+            reject();
+        })
+
+        .then(function() {
+            called = true;
+        });
+
+        setTimeout(function() {
+            if (!called) {
+                console.error('Test with id', id + ': Code too slow');
+                reject();
+            } else if (!success) {
+                reject();
+            } else {
+                if (!mark.marked[id]) {
+                    console.error('Test with id', id + ': mark service never called');
+                    reject();
+                } else {
+                    console.log('Test with id', id + ': Success')
+                    resolve();
+                }
+            }
+        });
     });
 }
 
 function testError(id, error) {
     return new Promise(function(resolve, reject) {
+        let promise = job();
+
+        if (!isPromise(promise)) {
+            console.error('Test with id', id + ': Result is not a promise');
+            reject();
+            return;
+        }
+
+        let success = false,
+            called = false;
+
+        job
+
+        .then(function() {
+            console.error('Test with id', id + ': The returned promise should be rejected with error', error);
+            reject();
+        })
+
+        .catch(function(data) {
+            if (error !== data) {
+                console.error('Test with id', id + ': The returned promise should be rejected with error', error);
+                reject();
+            } else {
+                success = true;
+            }
+        })
+
+        .then(function() {
+            called = true;
+        });
+
+        setTimeout(function() {
+            if (!called) {
+                console.error('Test with id', id + ': Code too slow');
+                reject();
+            } else if (!success) {
+                reject();
+            } else {
+                if (mark.marked[id]) {
+                    console.error('Test with id', id + ': mark service called');
+                    reject();
+                } else {
+                    console.log('Test with id', id + ': Success')
+                    resolve();
+                }
+            }
+        }, 225);
     });
+}
+
+function isPromise(obj) {
+    return obj instanceof Promise;
 }
