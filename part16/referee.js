3f54e9d89d688698d1408c6f5d48b238e26ed19c
David Reignier <david.reignier@gmail.com>
2017-06-12 19:43:31 +0200

    fix part16

--- part16/referee.js
+++ part16/referee.js
@@ -3,14 +3,14 @@ let job = require('./code'),
 
 Promise.all([
     testSuccess.apply(null, '1 jsmith USA John Smith jsmith@gmail.com'.split(' ')),
-    /*testSuccess.apply(null, '2 kmartin France Kevin Martin darkwarrior73@msn.fr'.split(' ')),
+    testSuccess.apply(null, '2 kmartin France Kevin Martin darkwarrior73@msn.fr'.split(' ')),
     testSuccess.apply(null, '3 egringo Mexico Erico Gringo elgringo@gmail.com'.split(' ')),
     testSuccess.apply(null, '4 gkruger Germany Gunter Kruger guntertag@hotmail.com'.split(' ')),
     testError(5, 'Error central'),
     testError(6, 'Error db1'),
     testError(7, 'Error db2'),
     testError(8, 'Error db3'),
-    testError(9, 'Error vault')*/
+    testError(9, 'Error vault')
 ])
 
 .then(function() {
@@ -37,7 +37,7 @@ function testSuccess(id, username, country, firstname, lastname, email) {
         let success = false,
             called = false;
 
-        job
+        promise
 
         .then(function(data) {
             success = true;
@@ -74,7 +74,7 @@ function testSuccess(id, username, country, firstname, lastname, email) {
         })
 
         .catch(function() {
-            console.error('Test with id', id + ': The should be resvoled');
+            console.error('Test with id', id + ': The returned promise should be resvoled');
             reject();
         })
 
@@ -114,7 +114,7 @@ function testError(id, error) {
         let success = false,
             called = false;
 
-        job
+        promise
 
         .then(function() {
             console.error('Test with id', id + ': The returned promise should be rejected with error', error);
