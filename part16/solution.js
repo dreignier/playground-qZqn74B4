a4f361e8bae3708c77bd4d065bc61f505260a6b3
David Reignier <david.reignier@gmail.com>
2017-06-20 09:11:32 +0200

    fix part16 solution

--- part16/solution.js
+++ part16/solution.js
@@ -5,54 +5,52 @@ let central = require('./central'),
     vault = require('./vault'),
     mark = require('./mark');
 
-module.exports = function(id) {
-    // Solution hidden. Unfold this block to see it.
-    { // autofold
-        let dbs = {
-            db1: db1,
-            db2: db2,
-            db3: db3
-        };
+// Solution hidden. Unfold this block to see it.
+module.exports = function(id) { // autofold
+    let dbs = {
+        db1: db1,
+        db2: db2,
+        db3: db3
+    };
 
-        return new Promise(function(resolve, reject) {
-            Promise.all([
-                central(id)
+    return new Promise(function(resolve, reject) {
+        Promise.all([
+            central(id)
 
-                .catch(function() {
-                    return Promise.reject('Error central');
-                })
-
-                .then(function(db) {
-                    return dbs[db](id)
-
-                    .catch(function() {
-                        return Promise.reject('Error ' + db);
-                    })
-                }),
-
-                vault(id)
-
-                .catch(function() {
-                    return Promise.reject('Error vault');
-                })
-            ])
-
-            .then(function(data) {
-                mark(id).catch(function() {});
-
-                resolve({
-                    id: id,
-                    username: data[0].username,
-                    country: data[0].country,
-                    firstname: data[1].firstname,
-                    lastname: data[1].lastname,
-                    email: data[1].email
-                });
+            .catch(function() {
+                return Promise.reject('Error central');
             })
 
-            .catch(function(error) {
-                reject(error);
+            .then(function(db) {
+                return dbs[db](id)
+
+                .catch(function() {
+                    return Promise.reject('Error ' + db);
+                })
+            }),
+
+            vault(id)
+
+            .catch(function() {
+                return Promise.reject('Error vault');
+            })
+        ])
+
+        .then(function(data) {
+            mark(id).catch(function() {});
+
+            resolve({
+                id: id,
+                username: data[0].username,
+                country: data[0].country,
+                firstname: data[1].firstname,
+                lastname: data[1].lastname,
+                email: data[1].email
             });
+        })
+
+        .catch(function(error) {
+            reject(error);
         });
-    }
+    });
 };
